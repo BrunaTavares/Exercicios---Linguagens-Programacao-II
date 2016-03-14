1cb0c9297e56f6cb5c5df283ee5362d7b68208d3
Gabriel <gcouto92@gmail.com>
2016-03-14 16:40:28 -0300

    exercicio 17 by ton

---
+++ Lista 2/Ex17/processar_req.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+if(count($_POST) > 0) {	
+		
+	echo "Dados submetidos por POST:" . "<br>";
+	foreach($_POST as $key=>$post_data) {
+		echo $key . " = " . $post_data . "<br>";
+	}
+}
+if(count($_GET) > 0) {
+	echo "Dados submetidos por GET:" . "<br>";
+	foreach($_GET as $key=>$get_data) {
+		echo $key . " = " . $get_data . "<br>";
+	}
+}
+
+
+
